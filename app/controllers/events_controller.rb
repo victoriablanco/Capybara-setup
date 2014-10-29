00a6893bb7c2b6ef67889f4bc85a63d4c1d559ea
Jeff Dean <jeff@galvanize.it>
2014-10-29 12:59:39 -0600

    initial commit with CRU for events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,45 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    event_params = params.require(:event).permit(
+      :description,
+      :location,
+      :date,
+      :capacity,
+      :requires_id
+    )
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to events_path
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    event_params = params.require(:event).permit(
+      :description,
+      :location,
+      :date,
+      :capacity,
+      :requires_id
+    )
+    @event.update(event_params)
+    redirect_to events_path
+  end
+
+end
